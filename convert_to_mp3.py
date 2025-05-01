a36eba4fa8cb3847d2916ea322fc655d7abfb7ee
AngeloMCF <angelo.filiage@gmail.com>
2025-05-01 01:30:14 -0300

    refactor: basic functions migrated to util.py, removed duplicated code

--- convert_to_mp3.py
+++ convert_to_mp3.py
@@ -1,13 +1,10 @@
 import os
 from datetime import datetime
-import time
+import util
+
+from default_config import diretorio_musicas_covertidas, diretorio_musicas_coverter, lista_formatos_aceitos
 
 from pydub import AudioSegment, effects
-
-
-def limpa_tela() -> None:
-    os.system('cls' if os.name == 'nt' else 'clear')
-
 
 def show_message(total_atual: int, total_max:int, file: str =None) -> None:
     print(f'Convertidos {total_atual} de {total_max} : {(total_atual/total_max)*100:.2f}%')
@@ -31,16 +28,6 @@ def listar_diretorio_por_tipo_arquivo(diname:str, file_types:list = None) -> lis
             if _type in _file: _list.append(_file)
 
     return _list
-
-
-def criar_diretorio_converteded(dir_path:str, dir_name:str) -> None:
-    if dir_name not in os.listdir(dir_path):
-        os.mkdir( os.path.join(dir_path, dir_name)) 
-
-
-def time_formated(tempo: time.localtime = time.localtime() ):
-    print(f'{tempo.tm_hour}h {tempo.tm_min} min {tempo.tm_sec} sec')
-
 
 def _infos(execucao: int, tempo: datetime, qnt_erro: int = 0, _time: int = 0, espaco:int = 10):
     """ Função para exibir informações das execuções. 
@@ -70,23 +57,17 @@ def _infos(execucao: int, tempo: datetime, qnt_erro: int = 0, _time: int = 0, es
 
     caratere = '-'
     _time_stamp = datetime.now() - tempo
-    tempo_formatado:str = '0:00:00.000000' #f'{tempo.hour} : {tempo.minute} {tempo.second}  {tempo.microsecond}'
-    mensagem =f"| Execução: {execucao} | Run time: {_time_stamp if _time_stamp.days>= 0 else tempo_formatado} | Erros: {qnt_erro} |"
+    tempo_formatado:str = '0:00:00.000000' 
+    mensagem =f"| Execução: {execucao} | Run time: {_time_stamp if _time_stamp.days >= 0 else tempo_formatado} | Erros: {qnt_erro} |"
     print(f'{caratere*len(mensagem)}')
     print(mensagem)
     print(f'{caratere*len(mensagem)}')
 
 
-def log_to_file(erro:str, texto:str, file_name:str = 'logs.txt'):
-    with open (file_name, 'w+') as _log_file:
-        m:str =f'Erro durante execucao: "{texto}", erro: {erro}' 
-        _log_file.write(m)
-
-
-def exportar_mp3(lista:list, dir_music:str, dir_eport_path:str,
-                equalizar_audio: bool = True, 
-                remover_silencio_bordas: bool = False,
-                renomear_arquivo_final: bool = True,
+def export_mp3_list(lista:list, dir_music:str, dir_eport_path:str,
+                equalize_audio: bool = True, 
+                remove_silence_edges: bool = False,
+                rename_final_file: bool = True,
                 arquivo_final_inicio: str = '_converted_'
                 ):
     qnt: int = len(lista)
@@ -94,58 +75,114 @@ def exportar_mp3(lista:list, dir_music:str, dir_eport_path:str,
     cc_erros: int = cc
     start_time = datetime.today()
 
+    log_file_name:str = f"{str(datetime.now())[:19].replace(':', '-').replace(' ', '-')}-logs.txt"
+
     for file in lista:
         _infos(cc + 1, start_time, cc_erros)
         show_message(cc, qnt, file)
+    
         try:
             _mp3file_path = os.path.join(dir_music, file)
-            _extension:str = file[-3::]
-            # mp3: AudioSegment = AudioSegment.from_mp3(_mp3file_path)
-            mp3: AudioSegment = AudioSegment.from_file(_mp3file_path, _extension)
-            print('\tArquivo extraído.')
-            
-            if equalizar_audio:
-                print('\tIniciando normalizção.')
-                mp3 = effects.normalize(mp3)
-                print('\tArquivo normalizado.')
-            
-            if remover_silencio_bordas:
-                print('\tRemovendo silêncio das bordas.')
-                mp3 = effects.strip_silence(mp3)
-                print('\tSilêncio removido.')
-            
-            print('\tExportando aquivo.')
 
-            nome_arquivo = f'{arquivo_final_inicio}{file[:-3]}.mp3' if renomear_arquivo_final else f'{file[:-3]}.mp3' 
-            caminho_arquivo_final:str = f'{dir_eport_path}/{nome_arquivo}'
-            mp3.export(caminho_arquivo_final, format='mp3')
-            print('\tArquivo exportando.')
+            file_name = f'{arquivo_final_inicio}{file[:-3]}.mp3' if rename_final_file else f'{file[:-3]}.mp3' 
+            file_name = file_name.replace('..', '.')
+            caminho_arquivo_final:str = os.path.join(dir_eport_path, file_name)
+
+            convert_mp3(_mp3file_path, caminho_arquivo_final, file, equalize_audio = equalize_audio, remove_silence_edges= remove_silence_edges)
+            util.log_to_file('', f'Info - exportado "{file_name}"', file_name=log_file_name, crititca=False)
+            
+            cc += 1
+
         except Exception as e:
             error_message:str = f'exec:{cc}'
-            log_to_file(e, error_message)
+            util.log_to_file(e, error_message, file_name=log_file_name)
             cc_erros += 1      
         
-        cc +=1
-        limpa_tela()
+        util.clean_scream()
     
     _infos(cc, start_time, cc_erros)
 
     show_message(cc, qnt)
+
+    if os.path.exists(f'logs/{log_file_name}'):
+        print(f'Arquivo de logs em: "./logs/{log_file_name}"')
+
+def convert_mp3(input_path:str, output_path:str, file:str, 
+                equalize_audio: bool = False, 
+                remove_silence_edges: bool = False) -> None:
+
+    try:
+        input_path = input_path.replace('./', '').replace('/', '\\')
+        output_path = output_path.replace('./', '').replace('/', '\\')
+
+        file_extension:str = file[-3::]
+        file_name:str = file.replace(file_extension, '').replace('.', '')
+        cwd:str= os.getcwd()
+        caminho:str = os.path.join(cwd, input_path, file)
+        caminho_arquivo_final:str = os.path.join(cwd, output_path,file_name + '.mp3' )
+
+        print('\tIniciando extracao.')
+        mp3: AudioSegment = AudioSegment.from_file(caminho, file_extension)
+        print('\tArquivo extraido.')
+
+        if equalize_audio:
+            print('\tIniciando normalizacao.')
+            mp3 = effects.normalize(mp3)
+            print('\tArquivo normalizado.')
+            
+        if remove_silence_edges:
+            print('\tRemovendo silencio das bordas.')
+            mp3 = effects.strip_silence(mp3)
+            print('\tSilencio removido.')
+        
+        print('\tExportando aquivo.')
+        mp3.export(caminho_arquivo_final, format='mp3')
+        print('\tArquivo exportado.\n')
+
+    except Exception as e:
+        print(f'erro convert_mp3: {e}')
  
 
 def run():
-    limpa_tela()
+    util.setup()
+    util.clean_scream()
 
-    diretorio_raiz:str = 'assets'
-    diretorio_musicas:str = os.path.join(diretorio_raiz, '_samples')
-    diretorio_musicas_convertidas:str = '_converted'
+    diretorio_musicas:str = diretorio_musicas_coverter
+    diretorio_musicas_convertidas:str = diretorio_musicas_covertidas
 
-    criar_diretorio_converteded(diretorio_raiz, diretorio_musicas_convertidas)
-    
-    arquivos:list =  listar_diretorio_por_tipo_arquivo(diretorio_musicas, ['.mp4', '.mp3'])
-    
-    exportar_mp3(arquivos,diretorio_musicas, os.path.join(diretorio_raiz, diretorio_musicas_convertidas), renomear_arquivo_final= False)
+    arquivos:list = listar_diretorio_por_tipo_arquivo(diretorio_musicas, lista_formatos_aceitos)
+
+    print(len(arquivos) > 0, len(arquivos), arquivos )
+
+    if len(arquivos) > 0: 
+        export_mp3_list(arquivos,diretorio_musicas,diretorio_musicas_convertidas, rename_final_file= False)
+    else:
+        print(f'Arquivo vazio: "{diretorio_musicas}"')
+
+@NotImplementedError
+def __force():
+
+    file_in  = r'' # full path 
+    file_out = r'' # full path 
+
+    try:
+        mp3: AudioSegment = AudioSegment.from_file(file_in, 'm4a')
+        print('\tArquivo extraído.')
+                
+        print('\tExportando aquivo.')
+
+        mp3.export(file_out, format='mp3')
+        print('\tArquivo exportado.')
+
+    except Exception as e:
+        print(e)
+
+    try:
+        os.remove(file_in)
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
     run()
+    # __force()
 
