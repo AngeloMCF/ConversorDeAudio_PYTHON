4f78701ffbba723604968d8ac3375c13db5ad840
AngeloMCF <angelo.filiage@gmail.com>
2025-04-30 23:48:17 -0300

    feat: add default configuration file, will be used on future

---
+++ default_config.py
@@ -0,0 +1,11 @@
+# converter musicas
+diretorio_musicas_coverter:str = './assets/convert'
+diretorio_musicas_covertidas:str = './assets/converted'
+
+lista_formatos_aceitos:list = ['.mp4', '.mp3', '.wma', '.m4a', '.mpeg', '.wmv']
+
+# download de audio ou video do yotube
+destino_video:str = './assets/video'
+destino_audio:str = './assets/audio'
+
+arquivo_links_baixar_youtube:str = './assets/lista_link_youtube'
